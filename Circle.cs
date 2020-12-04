8a17b9021fb70c2b090b44878fb9bd4a681e0306
Timothy <tlstewart22@gmail.com>
2020-12-04 12:43:38 -0600

    2.6 Studio: Area of a Circle

---
+++ Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaOfACircle
+{
+    public class Circle
+    {
+        public static double AreaCircle(double radius)
+        {
+            double area;
+            double PI = Math.PI;
+            area = radius * radius * PI;
+            return area;
+        }
+
+        public static double Circumference(double radius)
+        {
+            double circumference;
+            double PI = Math.PI;
+            circumference = 2 * PI * radius;
+            return circumference;
+        }
+
+        public static double Diameter(double radius)
+        {
+            double diameter;
+            double PI = Math.PI;
+            diameter = 2 * radius;
+            return diameter;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AreaOfACircle
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double radius;
+            double area;
+            string input;
+            double circumference;
+
+            Console.WriteLine("What is the radius? ");
+            input = Console.ReadLine();
+            radius = double.Parse(input);
+            area = Circle.AreaCircle(radius);
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + area);
+            circumference = Circle.Circumference(radius);
+            Console.WriteLine("The circumference of a circle with radius " + radius + " is: " + circumference);
+            double diameter = Circle.Diameter(radius);
+            Console.WriteLine("The diameter of the circle with the radius " + radius + " is: " + diameter);
+
+            // Console.WriteLine(Circle.AreaCircle(radius));
+
+            Console.WriteLine("What is your car's MPG?: ");
+            input = Console.ReadLine();
+            int mpg = int.Parse(input);  // 
+            double gallonsGasUsed = diameter / mpg;
+            Console.WriteLine("The car will use " + gallonsGasUsed + " gallons of gas going around the circle.");
+        }
+    }
+}
